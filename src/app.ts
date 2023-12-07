8028f7c1fcd05d7e3241520f980dc35807a23596
osama refae <85259135+osama-rf@users.noreply.github.com>
2023-12-08 02:55:35 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, {Request, Response} from "express";
+import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import {connectToDB} from "./api/utils/dbConfig";
+import movieRoutes from './api/routes/movieRoutes';
+import reservationRoutes from './api/routes/reservationRoutes';
+import {SuccessMessages} from "./api/utils/constants";
+import errorHandler from "./api/middlewares/errorHandler";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerOptions from './swagger';
+
+
+
+const app = express();
+
+// Global error handler middleware
+app.use(errorHandler);
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || 'your-mongo-uri';
+
+app.use(bodyParser.json());
+connectToDB(MONGO_URI)
+
+// Swagger UI
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/api/v1/", (req: Request, res: Response) => {
+    res.send("Hello Movies!");
+});
+
+app.use('/api/v1/movies', movieRoutes);
+app.use('/api/v1/reservations', reservationRoutes);
+
+const port = process.env.PORT || 'your-port';
+app.listen(port, () => console.log(SuccessMessages.ServerRunning));
+
+export default app;
